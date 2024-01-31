0a623e70a583b2bb8451f5cdb1eba90bba52e46b
zhouzijing <zhouzijing@xiaomi.com>
2024-01-31 17:33:53 +0800

    [Change] test用例

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,12 @@ import com.xiaomi.com.xiaomi.kotlinBsDiff.core.*
 import java.io.File
 
 fun main() {
-    val oldFileName = "com.tencent.mm_8.0.45.apk"
-    val newFileName = "com.tencent.mm_8.0.46.apk"
-//    val oldFileName = "oldFile"
-//    val newFileName = "newFile"
+    val oldFileName = "oldFile"
+    val newFileName = "newFile"
+    var currentTimeMillis = System.currentTimeMillis()
     KtApkDiff.diff(oldFileName, newFileName, "patch_2")
-    val currentTimeMillis = System.currentTimeMillis()
-//    KtApkPatch.patch(oldFileName, "patch_2", "new")
     println(System.currentTimeMillis() - currentTimeMillis)
-//    val zipFileAnalyzer = ZipFileAnalyzer(File(newFileName))
-//    val zipFileSolver = ZipFileSolver(zipFileAnalyzer)
-//    val unCompressFile = zipFileSolver.unCompressFile()
-//    println(unCompressFile.size)
+    currentTimeMillis = System.currentTimeMillis()
+    KtApkPatch.patch(oldFileName, "patch_2", "new")
+    println(System.currentTimeMillis() - currentTimeMillis)
 }
